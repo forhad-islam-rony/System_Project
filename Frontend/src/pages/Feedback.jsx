ea495a869a928d4152bc7bc1af33f878dc30306e
Apn7 <ekramul.alam21@gmail.com>
2024-12-09 04:07:08 +0600

    Add feedback form and enhance feedback display in Feedback component

--- Frontend/src/pages/Feedback.jsx
+++ Frontend/src/pages/Feedback.jsx
@@ -1,45 +1,75 @@
-import React from 'react'
-import avatar from '../assets/images/avatar-icon.png';
+import React from "react";
+import avatar from "../assets/images/avatar-icon.png";
 import { formateDate } from "../utils/formateDate";
+import { AiFillStar } from "react-icons/ai";
+import FeedbackForm from "./FeedbackForm";
 
 const Feedback = () => {
+  const [showFeedbackForm, setShowFeedbackForm] = React.useState(false);
+
   return (
     <div>
       <div className="mb-[50px]">
-        <h4 className="text-[20px] leading-[30px]
+        <h4
+          className="text-[20px] leading-[30px]
         font-bold text-headingColor
-        mb-[30px]">
+        mb-[30px]"
+        >
           All reviews (272)
         </h4>
 
-        <div className="flex justify-between
-        gap-10 mb-[30px]">
+        <div
+          className="flex justify-between
+        gap-10 mb-[30px]"
+        >
           <div className="flex gap-3">
             <figure className="w-10 h-10 rounded-full">
               <img className="w-full" src={avatar} alt="" />
             </figure>
 
             <div>
-              <h5 className='text-[16px] leading-6
-              text-primaryColor font-bold'>
+              <h5
+                className="text-[16px] leading-6
+              text-primaryColor font-bold"
+              >
                 Ali ahmed
               </h5>
-              <p className="text-[14px] leading-6
-              text-textColor">
+              <p
+                className="text-[14px] leading-6
+              text-textColor"
+              >
                 {formateDate("02-14-2023")}
               </p>
-              <p className='text_para mt-3 font-medium
-              text-[15px]'>
-                Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua.
+              <p
+                className="text_para mt-3 font-medium
+              text-[15px]"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </div>
           </div>
+
+          <div className="flex gap-1">
+            {[...Array(5).keys()].map((_, index) => (
+              <AiFillStar key={index} color="#0067FF" />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
 
-export default Feedback
+      {!showFeedbackForm && (
+        <div className="text-center">
+          <button className="btn" onClick={() => setShowFeedbackForm(true)}>
+            Give Feedback
+          </button>
+        </div>
+      )}
+
+      {showFeedbackForm && <FeedbackForm />}
+
+    </div>
+  );
+};
+
+export default Feedback;
