56bdf8bf97e2a03809b408e520aa635eb79c978e
Forhad Islam Rony <forhadrony161@gmail.com>
2024-12-09 11:37:54 +0600

    Reviews backend implemeted

--- Backend/Controllers/reviewController.js
+++ Backend/Controllers/reviewController.js
@@ -12,6 +12,7 @@ export const getAllReviews = async (req, res) => {
 }
 
 export const createReview = async (req, res) => { 
+  
   if(!req.body.doctor) req.body.doctor = req.params.doctorId;
   if(!req.body.user) req.body.user = req.userId;
   const newReview = new Review(req.body);
@@ -21,7 +22,7 @@ export const createReview = async (req, res) => {
 
     await Doctor.findByIdAndUpdate(req.body.doctor, {
       $push: {reviews: savedReview._id}
-    }, {new: true});
+    });
 
     res.status(200).json({success: true, message: 'Review created successfully', data: savedReview});
   }
